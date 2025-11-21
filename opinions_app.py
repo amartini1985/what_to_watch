b8d07fdf17807bf2f5efa7e76bd57ec0590ee183
Andrey Martyanov <martyanovav@yandex.ru>
2025-11-21 15:14:36 +0300

    Add db

--- opinions_app.py
+++ opinions_app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from pprint import pprint
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index_view():
+    # pprint(app.config)
     return 'Совсем скоро тут будет случайное мнение о фильме!'
 
 if __name__ == '__main__':
