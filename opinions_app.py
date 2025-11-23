186a871a535117c210d6892d78735d0c4defa9e3
Andrey Martyanov <martyanovav@yandex.ru>
2025-11-23 18:44:13 +0300

    Add migrate

--- opinions_app.py
+++ opinions_app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+# what_to_watch/opinions_app.py
+
 from datetime import datetime
-from pprint import pprint
 from random import randrange
 
-app = Flask(__name__, static_folder='static')
+from flask import Flask, redirect, render_template, url_for, flash, abort
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, URLField
+from wtforms.validators import DataRequired, Length, Optional
+
+
+app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SECRET_KEY'] = 'MY SECRET KEY'
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Opinion(db.Model):
@@ -19,24 +29,78 @@ class Opinion(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
+class OpinionForm(FlaskForm):
+    title = StringField(
+        'Введите название фильма',
+        validators=[DataRequired(message='Обязательное поле'), Length(1, 128)],
+    )
+    text = TextAreaField(
+        'Напишите мнение',
+        validators=[DataRequired(message='Обязательное поле')],
+    )
+    source = URLField(
+        'Добавьте ссылку на подробный обзор фильма',
+        validators=[Length(1, 256), Optional()],
+    )
+    submit = SubmitField('Добавить')
+
+
 @app.route('/')
 def index_view():
-    opinions = Opinion.query.count()
-    if not opinions:
-        return 'В базе данных нет фильмов'
-    value = randrange(opinions)
-    opinion = Opinion.query.offset(value).first()
+    quantity = Opinion.query.count()
+    if not quantity:
+        abort(500)
+    offset_value = randrange(quantity)
+    opinion = Opinion.query.offset(offset_value).first()
     return render_template('opinion.html', opinion=opinion)
 
-@app.route('/add')
+
+@app.route('/add', methods=['GET', 'POST'])
 def add_opinion_view():
-    return render_template('add_opinion.html')
+    form = OpinionForm()
+    if form.validate_on_submit():
+        text = form.text.data
+        # Если в БД уже есть мнение с текстом, который ввёл пользователь...
+        if Opinion.query.filter_by(text=text).first() is not None:
+            # ...вызвать функцию flash и передать соответствующее сообщение.
+            flash('Такое мнение уже было оставлено ранее!')
+            # Вернуть пользователя на страницу «Добавить новое мнение».
+            return render_template('add_opinion.html', form=form)
+        opinion = Opinion(
+            title=form.title.data, 
+            text=text, 
+            source=form.source.data
+        )
+        db.session.add(opinion)
+        db.session.commit()
+        return redirect(url_for('opinion_view', id=opinion.id))
+    return render_template('add_opinion.html', form=form)
 
-@app.route('/opinions/<int:id>')  
-def opinion_view(id):  
-    opinion = Opinion.query.get_or_404(id)  
 
-    return render_template('opinion.html', opinion=opinion) 
+@app.route('/opinions/<int:id>')
+def opinion_view(id):
+    opinion = Opinion.query.get_or_404(id)
+    return render_template('opinion.html', opinion=opinion)
+
+@app.errorhandler(500)
+def internal_error(error):
+    # Ошибка 500 возникает в нештатных ситуациях на сервере. 
+    # Например, провалилась валидация данных.
+    # В таких случаях можно откатить изменения, незафиксированные в БД,
+    # чтобы в базу не записалось ничего лишнего.
+    db.session.rollback()
+    # Пользователю вернётся страница, сгенерированная на основе шаблона 500.html.
+    # Этого шаблона пока нет, но сейчас вы его тоже создадите.
+    # Пользователь получит и код HTTP-ответа 500.
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    # При ошибке 404 в качестве ответа вернётся страница, созданная
+    # на основе шаблона 404.html и код HTTP-ответа 404.
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run()
+    
